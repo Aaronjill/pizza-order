4dbb855d600a1c48012a9272e948aa3a9c53b285
Aaronjill <aaronjillundra@gmail.com>
2022-05-19 14:44:21 +0530

    pizza order

--- PIZZA ORDER.py
+++ PIZZA ORDER.py
@@ -9,12 +9,14 @@ if size == 'S':
     bill += 2
 elif size == 'M':
   bill+= 20
-  if add_pepperoni =='Y':
-    bill+=3
 elif size == 'L':
   bill+= 25
-  if add_pepperoni =='Y':
-    bill+=3
+  
+if add_pepperoni == 'Y':
+  if size == 'S':
+    bill += 2
+  else:
+    bill += 3
 if extra_cheese == 'Y':
   bill += 1
 print('your total bill is',bill)
